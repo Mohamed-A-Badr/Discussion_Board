233749072de4ef088f3dfd1274a9fd3ac280a512
Mohamed-A-Badr <badr49960@gmail.com>
2022-10-30 01:17:47 +0200

    Creating home page

--- Boards/url.py
+++ Boards/url.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns =[
-    path('', views.HomeView.as_view(), name='home')
+    path('', views.HomeView.as_view(), name='home'),
+    path('boards/<int:board_id>/', views.BoardView.as_view(), name='board_topics')
 ]
--- Boards/views.py
+++ Boards/views.py
@@ -8,3 +8,12 @@ class HomeView(View):
     boards = BoardData.objects.all()
     def get(self, request):
         return render(request, 'home.html', {'boards': self.boards})
+
+class BoardView(View):
+    def get(self, request, board_id):
+        board = get_object_or_404(BoardData, pk=board_id)
+        topics = board.topic.all()
+        return render(request, 'topic.html', {
+            'board':board,
+            'topics':topics,
+        })
